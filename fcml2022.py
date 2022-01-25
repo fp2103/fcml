d4d3354c9ba625d5954745e418d6ae87d369bf10
fpietri <pietri.fred@gmail.com>
2022-01-25 22:50:46 +0100

    prepare for matrice

--- fcml2022.py
+++ fcml2022.py
@@ -5,26 +5,28 @@
 Solve Freecell with tree search and evolutionary algorithm on best solvers
 """
 
-import random
 import sys
 import freecell as fc
 import random
 
 MAX_ITER_DEPTH = 2000
-PARAMS = 6
+PARAMS = 7
+IDENTITY_STATE = (1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0)
 SORTED_DECK = [fc.Card(j, i) for j in fc.SUITS for i in range(1, len(fc.CARD_VALUE)+1)]
 
 class StateData(object):
-    def __init__(self, state, weight, choice, choice_hash):
+    def __init__(self, state, weight, choice, choice_hash, weight_vector):
         self.state = state
         self.weight = weight
         self.choice = choice
         self.choice_hash = choice_hash
+        self.weight_vector = weight_vector
 
 class Solver(object):
     def __init__(self, init_state, strategy):
         self.init_state = init_state
         self.strategy = strategy
+        self._coeff_sum = sum(strategy)
 
         self._iter = 0
         self._path = []
@@ -41,21 +43,28 @@ class Solver(object):
             col_dest_str = ",".join([str(c.uid) for c in state.columns[choice.col_dest]])
         ret += "-".join(sorted([col_orig_str, col_dest_str]))
         return ret
-
-
-    def _weight_state(self, state):
-        """ .in base/52
+    
+    @staticmethod
+    def compute_state_weight(state):
+        """ # primary measures:
+            .in base/52
+            .gap between bases
             .in serie/remaining
             .max_mvt/13 (max 1)
+            # secondary measures:
             .to base/choices
             .from fc/choices
             .free col & not for fc/choices
         """
         if state.is_won:
-            return 1.0
+            return IDENTITY_STATE
 
-        in_base = sum([len(state.bases.get(k)) for k in fc.SUITS])
+        bases_len = [len(state.bases.get(k)) for k in fc.SUITS]
+        in_base = sum(bases_len)
         in_base_norm = in_base / 52.0
+
+        # negative value, as high value = bad
+        gap_bases = (min(bases_len) - max(bases_len)) / 13.0
 
         in_serie = sum([len(state.column_series[i]) for i in range(0, fc.COLUMN)]) / (52.0 - in_base)
 
@@ -77,16 +86,23 @@ class Solver(object):
             from_fc = from_fc / len(choices)
             free_col = free_col / len(choices)
 
-        state_weight = (in_base_norm, in_serie, mvt, to_base, from_fc, free_col)
+        return (in_base_norm, gap_bases, in_serie, mvt, to_base, from_fc, free_col)
+        
+    def _weight_state(self, weight_vector, curr_state_wv):
+        # Won
+        if weight_vector == IDENTITY_STATE:
+            return 2.0
 
         ret = 0
         for i in range(PARAMS):
-            ret += state_weight[i]*self.strategy[i]
-        return float(ret/PARAMS)
+            ret += weight_vector[i]*self.strategy[i]
+        return float(ret/self._coeff_sum)
 
     
     def solve(self):
-        self._play(StateData(self.init_state, self._weight_state(self.init_state), None, ""), -2)
+        swv = Solver.compute_state_weight(self.init_state)
+        sw = self._weight_state(swv, IDENTITY_STATE)
+        self._play(StateData(self.init_state, sw, None, "", swv), -2.0)
         print(self._iter)
         return self._path
 
@@ -117,7 +133,9 @@ class Solver(object):
                 continue
           
             ns = state_data.state.apply(c)
-            next_states_data.append(StateData(ns, self._weight_state(ns), c, hc))
+            swv = Solver.compute_state_weight(ns)
+            sw = self._weight_state(swv, state_data.weight_vector)
+            next_states_data.append(StateData(ns, sw, c, hc, swv))
         next_states_data.sort(key=lambda x: x.weight, reverse=True)
 
         # recurse on next states, stop when won or if state is not best
@@ -134,8 +152,9 @@ class Solver(object):
         
 
 def solve_game(game):
-    solvers = [(1.0,0.5,1.0,0.0,1.0,0.0)]
-    solvers.extend([(random.random(),random.random(),random.random(),random.random(),random.random(),random.random()) for _ in range(99)])
+    solvers = [(1.0,0,0.5,1.0,0.0,1.0,0.0)]
+    #solvers = [(0.19907451603213988, 0.38189679351526395, 0.9931985466148217, 0.7580500957653592, 0.13096734350362638, 0.3660447641233754)]
+    solvers.extend([(random.random(),random.random(),random.random(),random.random(),random.random(),random.random(),random.random()) for _ in range(99)])
     
     for s in solvers:
         print(s)
